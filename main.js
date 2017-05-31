1268832132b1c74c70ed2f908cd27028657b6a61
Taneisha Mitchell-Gayle <TMitchellGayle@realdecoy.com>
2017-05-30 21:52:16 -0500

    Electron install & main.js file creation

---
+++ main.js
@@ -0,0 +1,16 @@
+const {
+  app,
+  BrowserWindow
+} = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1000,
+    height: 200
+  });
+  win.
+}
