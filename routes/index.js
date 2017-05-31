44982c55947f420064352c99ebcb61584bf257a4
Taneisha Mitchell-Gayle <TMitchellGayle@realdecoy.com>
2017-05-30 21:09:52 -0500

    REGEX & Market/Live Update Corrections

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,56 @@
 var express = require('express'),
-    jsonfile = require('jsonfile'),
-    request = require('request'),
-    path = require('path');
-    cheerio = require('cheerio'),
-    fs = require('fs');
+  jsonfile = require('jsonfile'),
+  request = require('request'),
+  path = require('path');
+cheerio = require('cheerio'),
+  fs = require('fs');
 
 var router = express.Router();
 
-
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 var dest = './public/jse.json';
 var osmosis = require('osmosis');
-var url = 'https://www.jamstockex.com';
-
+//var url = 'https://www.jamstockex.com';
+var url = 'https://www.jamstockex.com/ticker-data';
 osmosis.get(url)
-.set({
-  //title: 'title',
-  struct: [
-    osmosis
-    .find('div.marquee')
-    .set({
-      'ticker': 'ul',
-      //'link': 'li a',
-    })
-  ]
-})
-.data(function(document) {
-  console.log(document);
-  var obj = JSON.stringify(document);
-  obj = obj.replace(/[\s|\\n]+/g, ' ').match(/[A-Z]*[^A-Z]+/g);
-  //obj = obj.match(/[A-Z]*[^A-Z]+/g);
-  jsonfile.writeFileSync(dest, obj);
-});
-//
-// $(window).scroll(function () {
-//    if ($(window).scrollTop() >= $(obj).height() - $(window).height() - 10) {
-//       //Add newly-crunched data at the end of the page
-//    }
-// });
+  .set({
+    //title: 'title',
+    struct: [
+      osmosis
+      //.find('div.marquee')
+      .find('ul')
+      .set({
+        //'ticker': 'ul',
+        'ticker': 'ul',
+      })
+    ]
+  })
+  .data(function(document) {
+    var obj = JSON.stringify(document); //get json info & convert it to a string
+    obj = obj.replace(/[\s|\\n]+/g, ' ').match(/[A-Z]*[^A-Z]+/g); //remove all newline & extra spaces (NOTE: need to be able to leave 'n' in place it is currently being stripped)
+
+    var item = 'Market ';
+
+    // let matches = obj.filter(s => s.includes(item))
+    // console.log(matches);
+    //var isTrue = console.log(obj.includes(item));
+
+    if (obj.includes(item)) {
+      console.log('Entered MARKET!');
+      obj = obj.slice(34);
+    } else {
+      console.log('We LIVE!');
+      obj = obj.slice(2);
+    }
+
+    jsonfile.writeFileSync(dest, obj);
+  });
+
 module.exports = router;
